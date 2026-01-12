32a4a580903f18f64e7ffd80a43544c72cd8eb73
lee-jaemmin <dlwoals8546@gmail.com>
2026-01-12 18:19:43 +0900

    [FEAT] user_repo init

---
+++ lib/class/appuser_repo.dart
@@ -0,0 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:prost/class/app_user.dart';
+
+class UserRepository {
+  final FirebaseFirestore _db;
+  final auth.FirebaseAuth _auth;
+
+  UserRepository({
+    FirebaseFirestore? db,
+    auth.FirebaseAuth? authInstance,
+  }) : _db = db ?? FirebaseFirestore.instance,
+       _auth = authInstance ?? auth.FirebaseAuth.instance;
+  // init list: set field at a time when instance is created
+
+  /// find path to store data(maybe AppUser instance)
+  DocumentReference<Map<String, dynamic>> _meDoc() {
+    final uid = _auth.currentUser?.uid;
+    if (uid == null) {
+      throw StateError('로그인이 필요합니다. ERROR CODE: currentUser is null');
+    }
+    return _db.collection('users').doc(uid);
+  }
+
+  /// 이미 있는 데이터: 냅둠, 새 데이터: db에 등록
+  Future<void> upsertFromAuth({
+    required String company,
+    required String userName,
+  }) async {
+    final currentUser = _auth.currentUser;
+    if (currentUser == null)
+      throw StateError('로그인이 필요합니다. ERROR CODE: currentUser is null');
+
+    // create model using company, userName from outside
+    final appUser = AppUser.fromFirebase(
+      currentUser,
+      company: company,
+      userName: userName,
+    );
+
+    final ref = _db.collection('users').doc(currentUser.uid);
+    final snap = await ref.get();
+
+    if (!snap.exists) {
+      // 신규 가입 시 모든 정보 저장
+      await ref.set({
+        ...appUser.toMap(),
+        //...을 통해 내용물만 그대로 저장 가능: 중첩 방지
+      });
+    } else {
+      // 바뀔 수 있을 만한 정보만 업데이트
+      await ref.update({
+        'userName': userName,
+        'company': company,
+      });
+    }
+  }
+}
