58a8266179cefede1ad05615549664d3a46ae91e
lee-jaemmin <dlwoals8546@gmail.com>
2026-01-13 13:49:08 +0900

    [FEAT] table.dart init

---
+++ lib/class/table.dart
@@ -0,0 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+
+class TableModel {
+  final String tid;
+  final String tablename;
+  final String section;
+  final String status;
+  final String customer;
+  final String phonenumber;
+  final String staff;
+  final String bottle;
+  final DateTime createdat;
+
+  const TableModel({
+    required this.tid,
+    required this.tablename,
+    required this.section,
+    required this.status,
+    required this.customer,
+    required this.phonenumber,
+    required this.staff,
+    required this.bottle,
+    required this.createdat,
+  });
+
+  TableModel copyWith({
+    String? tablename,
+    String? section,
+    String? status,
+    String? customer,
+    String? phonenumber,
+    String? staff,
+    String? bottle,
+    DateTime? createdat,
+  }) {
+    return TableModel(
+      tid: this.tid,
+      tablename: tablename ?? this.tablename,
+      section: section ?? this.section,
+      status: status ?? this.status,
+      customer: customer ?? this.customer,
+      phonenumber: phonenumber ?? this.phonenumber,
+      staff: staff ?? this.staff,
+      bottle: bottle ?? this.bottle,
+      createdat: createdat ?? this.createdat,
+    );
+  }
+
+  factory TableModel.fromMap(String tid, Map<String, dynamic> map) {
+    return TableModel(
+      tid: tid,
+      tablename: map['tablename'] ?? '테이블 미지정',
+      section: map['section'] ?? '섹션 미지정',
+      status: map['status'] ?? '상태 미지정',
+      customer: map['customer'] ?? '손님 미지정',
+      phonenumber: map['phonenumber'] ?? '번호 없음',
+      staff: map['staff'] ?? '스태프 미지정',
+      bottle: map['bottle'] ?? '바틀 미지정',
+      createdat: (map['createdat'] as Timestamp?)?.toDate() ?? DateTime.now(),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'tablename': tablename,
+      'section': section,
+      'status': status,
+      'customer': customer,
+      'phonenumber': phonenumber,
+      'staff': staff,
+      'bottle': bottle,
+      'createdat': createdat,
+    };
+  }
+}
