2eb906bb72f65fd4c44aa02b3485d9f66bf5368c
lee-jaemmin <dlwoals8546@gmail.com>
2026-01-15 22:35:50 +0900

    [FEAT] table_res init

---
+++ lib/screens/table_reservation_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TableReservationScreen extends StatefulWidget {
+  const TableReservationScreen({super.key});
+
+  @override
+  State<TableReservationScreen> createState() => _TableReservationScreenState();
+}
+
+class _TableReservationScreenState extends State<TableReservationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('예약 화면'),
+    );
+  }
+}
