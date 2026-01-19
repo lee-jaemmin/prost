a9d17c79aeb181da1df7b9add02012eac0c5bf31
lee-jaemmin <dlwoals8546@gmail.com>
2026-01-19 14:06:58 +0900

    [FEAT] table_gridview init

---
+++ lib/widgets/table_gridview.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:prost/class/table.dart';
+import 'package:prost/class/table_repo.dart';
+import 'package:prost/widgets/table_card.dart';
+
+class TableGridView extends StatelessWidget {
+  final String company;
+  final String section;
+  final TableRepository _repo = TableRepository();
+
+  TableGridView({required this.company, required this.section});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<List<TableModel>>(
+      stream: _repo.getTablesStream(company, section), // 실시간 데이터 수신
+      builder: (context, snapshot) {
+        if (snapshot.hasError)
+          return Center(child: Text("오류 발생: snapshot.hasError"));
+        if (!snapshot.hasData)
+          return Center(child: CircularProgressIndicator());
+
+        final tables = snapshot.data!;
+
+        return GridView.builder(
+          padding: EdgeInsets.all(10),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, // 한 줄에 3개씩 배치
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+          ),
+          itemCount: tables.length,
+          itemBuilder: (context, index) {
+            final table = tables[index];
+            return TableCard(table: table);
+          },
+        );
+      },
+    );
+  }
+}
