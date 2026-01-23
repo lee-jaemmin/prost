1fbccb5f837b0a3a5e50152fe443b0e5ea2e42be
lee-jaemmin <dlwoals8546@gmail.com>
2026-01-23 19:45:26 +0900

    [ADD] 테이블 등록 Alert 연결

--- lib/widgets/table_card.dart
+++ lib/widgets/table_card.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:prost/class/table.dart';
+import 'package:prost/widgets/info_alert.dart';
 
 class TableCard extends StatelessWidget {
   final TableModel table;
+  final String companyId;
 
-  TableCard({required this.table});
+  TableCard({required this.table, required this.companyId});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: table.status == 'available' ? Colors.grey[200] : Colors.amber[100],
+      color: table.status == 'available'
+          ? Colors.grey[200]
+          : const Color.fromARGB(255, 230, 207, 200),
       child: InkWell(
         onTap: () {
-          /* 클릭 시 바틀 상세 정보 팝업 */
+          showDialog(
+            context: context,
+            builder: (context) {
+              return InfoAlert(companyId: companyId, table: table);
+            },
+          );
         },
         child: Padding(
           padding: EdgeInsets.all(8.0),
